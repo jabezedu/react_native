07aaab7b4c735df980dcffbbcfb2f06f836ee5e3
jabezedu <edujabez@gmail.com>
2025-09-06 08:38:57 +0530

    Added a image into it !

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, SafeAreaView , Image} from 'react-native';
 
 export default function App() {
   
   console.log("Did some changes ! ")
+//
   
   const handlePress = () => {
     console.log("This text has been clicked ! ")
@@ -14,7 +15,12 @@ export default function App() {
       <Text numberOfLines={1} onPress={()=> console.log("Text Clicked ")}>
         Hello World This is a test from Jabez - A readlly really long text and I wanna make it some more long
       </Text>
-      <StatusBar style="auto" />
+      <Image 
+        blurRadius={10}
+        fadeDuration={1000}
+        source={{
+        width : 200, height : 200,
+        uri : "https://picsum.dev/300/200"}}></Image>
     </SafeAreaView>
   );
 }
@@ -23,6 +29,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "dodgerblue",
+    justifyContent : "center",
+    alignItems : "center"
   },
 });
  
